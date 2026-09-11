5e67581a7d03055e007eb414e516ae22064ea49e
JhanisYPS <janainayasminps@gmail.com>
2026-09-10 22:52:14 -0300

    feat: add authentication and order routes with corresponding endpoints

--- api/Projeto_Pizzaria/main.py
+++ api/Projeto_Pizzaria/main.py
@@ -2,4 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from routes.auth_routes import auth_router
+from routes.order_routes import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
+
 # Rodar aplicação: python -m uvicorn main:app --reload
