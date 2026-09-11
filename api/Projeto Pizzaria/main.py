c908ed386f33391eb1f2d3442a923141523d89dd
JhanisYPS <janainayasminps@gmail.com>
2026-09-10 22:06:11 -0300

    feat: set up FastAPI application with initial requirements

--- api/Projeto Pizzaria/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-# Rodar aplicação: uvicorn main:app --reload
---
+++ api/Projeto_Pizzaria/main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Rodar aplicação: python -m uvicorn main:app --reload
