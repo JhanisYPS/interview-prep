76a10d26413d6a412fc4cab738968cc1ad0781c2
JhanisYPS <janainayasminps@gmail.com>
2026-09-24 18:51:29 -0300

    feat: update user creation logic and add UserSchema for validation

--- api/Projeto_Pizzaria/routes/auth_routes.py
+++ api/Projeto_Pizzaria/routes/auth_routes.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models.models import User
 from dependencies.dependencies import get_session
 
 from main import bcrypt_context
+from schemas.schemas import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -12,14 +14,14 @@ async def get_auth():
     return {"message": "Authentication endpoint"}
 
 @auth_router.post("/create_account")
-async def post_create_account(name:str, email: str, password: str, session = Depends(get_session)):
+async def post_create_account(user_schema: UserSchema, session: Session = Depends(get_session)):
 
-    user = session.query(User).filter(User.email==email).first()
+    user = session.query(User).filter(User.email==user_schema.email).first()
     if user:
-        return {'mensage': "user alredy existe"}
+        raise HTTPException(status_code=400, detail="user alredy exist")
     else:
-        crypt_password = bcrypt_context.hash(password)
-        new_user = User(name= name, email=email, password=crypt_password)
+        crypt_password = bcrypt_context.hash(user_schema.password)
+        new_user = User(name= user_schema.name, email=user_schema.email, password=crypt_password, active=user_schema.active, admin=user_schema.admin )
         session.add(new_user)
         session.commit()
         return {'mensage': "user created"}
