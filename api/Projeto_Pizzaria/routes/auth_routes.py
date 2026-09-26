5b58811054c2086d973ea42d6af4771d9347efa3
JhanisYPS <janainayasminps@gmail.com>
2026-09-26 11:48:24 -0300

    feat: add login endpoint and LoginSchema for user authentication

--- api/Projeto_Pizzaria/routes/auth_routes.py
+++ api/Projeto_Pizzaria/routes/auth_routes.py
@@ -3,10 +3,15 @@ from models.models import User
 from dependencies.dependencies import get_session
 
 from main import bcrypt_context
-from schemas.schemas import UserSchema
+from schemas.schemas import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+#função auxiliar
+def create_token(user_id:int):
+    token = f"ahdushduah{user_id}sudha"
+    return token
 
 @auth_router.get("/")
 async def get_auth():
@@ -25,4 +30,16 @@ async def post_create_account(user_schema: UserSchema, session: Session = Depend
         session.add(new_user)
         session.commit()
         return {'mensage': "user created"}
+
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.email==login_schema.email).first()
+    if not user:
+        raise HTTPException(status_code=400, detail="user not found")
+    else:
+        access_token = create_token(user.id)
+        return {
+            "access_token": access_token,
+            "token_type": "Bearer"
+        }
 
